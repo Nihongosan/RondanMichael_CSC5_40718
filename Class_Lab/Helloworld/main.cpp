3cbf1b6c49e7d84b8e01ce0dfc0e5c5c679dbc41
Nihongosan <michaelrondan@yahoo.com>
2015-01-06 20:03:03 -0800

    Signed-off-by: Nihongosan <michaelrondan@yahoo.com>

---
+++ Class_Lab/Helloworld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+  File:   main.cpp
+  Author: Michael Rondan
+  Created on January 6, 2015, 6:05 PM
+ Purpose: Our first program
+ */
+
+//system libraries
+#include <iostream>
+using namespace std;
+
+//User Libaries
+  
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
